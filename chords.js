94c52ae52425972de76db612be9417007d596f1c
Vincent <vinz2007@hotmail.com>
2019-11-24 16:03:41 +0100

    test responsive

--- chords.js
+++ chords.js
@@ -47,12 +47,14 @@ var fill = d3.scale.ordinal()
     .domain(d3.range(NameProvider.length))
     .range(colors);
 	
+var test = Math.min(700, window.innerWidth)
+var test2 = Math.min(500, 5 / 7 * window.innerWidth)
 /*//////////////////////////////////////////////////////////
 /////////////// Initiate Chord Diagram /////////////////////
 //////////////////////////////////////////////////////////*/
-var margin = {top: 30, right: 25, bottom: 10, left: 25},
-    width =700 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom,
+var margin = { top: 30, right: 25, bottom: 10, left: 25 },
+	width = test - margin.left - margin.right,
+	height = test2 - margin.top - margin.bottom,
     innerRadius = Math.min(width, height) * .36,
     outerRadius = innerRadius * 1.04;
 
